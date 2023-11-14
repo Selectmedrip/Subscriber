aef5612ccbc48a9f7d13cb6848ea59be76621c78
Selectmedrip <101542444+Selectmedrip@users.noreply.github.com>
2023-11-14 11:43:48 +0300

    v4  new UI icon in error subs
    Signed-off-by: Selectmedrip <101542444+Selectmedrip@users.noreply.github.com>

--- main.js
+++ main.js
@@ -21,10 +21,14 @@ document.getElementById("myBtn").addEventListener("click", function() {
     var email = document.querySelector(".in-put").value;
     var message = document.querySelector(".modal-content p");
     var title = document.querySelector(".modal-content h1");
-   var emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+    var emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+    var image = document.querySelector(".modal-content img");
+    
     if(emailRegex.test(email)) { title.textContent = "Спасибо за подписку!";
         message.innerHTML = "Письмо с подтверждением было отправлено на адрес: <pre>\t\t" + email + " </pre> Пожалуйста, откройте его и нажмите кнопку внутри, чтобы подтвердить подписку.";
+                                image.src = "check.svg";
     } else { title.textContent = "Подписка не выполнена!";
         message.textContent = "Пожалуйста, введите действительный адрес электронной почты.";
+            image.src = "svgrepo.svg";
     }
 });
